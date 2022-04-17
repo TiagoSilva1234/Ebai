eb3e0e3d081d1b6130319b77f7a7f91d365b56bc
Tiago SIlva <tigaonline12@gmail.com>
2022-04-17 22:20:36 +0100

    for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from './components/header/index.tsx';
+import Footer from './components/footer/index.tsx'
 function App() {
   return (
-    <>
+    <div className="App">
   <Header/>
    <Outlet/>
-</>
+   <Footer />
+</div>
   );
 }
 
