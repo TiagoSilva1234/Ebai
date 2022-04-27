cc8e048436c4d448765e6adb89afd8871878e20e
Tiago SIlva <tigaonline12@gmail.com>
2022-04-27 09:58:42 +0100

    search done

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
-import "./index.css"
+import React,{useState} from 'react';
+import "./index.scss"
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
+import {Link} from 'react-router-dom'
 const Search = ({placeholder,data})=>{
 
-    return <div className="search">
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+  
+    const handleFilter = (event) => {
+      const searchWord = event.target.value;
+      setWordEntered(searchWord);
+      const newFilter = data.filter((value) => {
+        return value.name.toLowerCase().includes(searchWord.toLowerCase());
+      });
+  
+      if (searchWord === "") {
+        setFilteredData([]);
+      } else {
+        setFilteredData(newFilter);
+      }
+    };
+  
+    const clearInput = () => {
+      setFilteredData([]);
+      setWordEntered("");
+    };
+  
+    return (
+      <div className="search">
         <div className="searchInputs">
-        <input className="input" type="text" placeholder={placeholder}/>
-        <div className="searchIcon"></div>
+          <input
+            type="text"
+            placeholder={placeholder}
+            value={wordEntered}
+            onChange={handleFilter}
+          />
+          <div className="searchIcon">
+            {filteredData.length === 0 ? (
+              <SearchIcon />
+            ) : (
+              <CloseIcon id="clearBtn" onClick={clearInput} />
+            )}
+          </div>
         </div>
-        <div className="dataResult"></div>
-    
-    </div>
-
-}
+        {filteredData.length != 0 && (
+          <div className="dataResult">
+            {filteredData.slice(0, 15).map((value, key) => {
+              return (
+                <Link className="dataItem"  to={"/product/"+value.id}>
+                  <p>{value.name} </p>
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    );
+  }
 export default Search;
